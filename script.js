c0f6891f78cc6fe23175c9132db6b8ea46b2208c
Moran Habooba <moranhabooba@gmail.com>
2023-07-16 11:27:12 +0300

    ADD Eraser

--- script.js
+++ script.js
@@ -30,12 +30,12 @@ function endPosition() {
 
 function draw(e) {
   if (!painting) return;
-  // console.log(e.offsetX, e.offsetY);
   ctx.lineWidth = lineWidth;
   ctx.lineCap = "round";
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
 }
+
 function startPositionTouch(e) {
   if (e.touches && e.touches.length > 0) {
     const touch = e.touches[0];
@@ -81,15 +81,45 @@ toolbar.addEventListener("change", (e) => {
 
     lineWidth = e.target.value;
   }
-});
+  if (e.target.id === "eraserSize") {
+    lineWidth = parseInt(e.target.value);
 
-toolbar.addEventListener("click", (e) => {
-  if (e.target.id === "Fill") {
-    ctx.fillStyle = color.value;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    lineWidth = e.target.value;
   }
 });
 
-// canvas.addEventListener("mousedown", startPosition);
-// canvas.addEventListener("mouseup", endPosition);
-// canvas.addEventListener("mousemove", draw);
+const fillImage = document.getElementById("fillImage");
+fillImage.addEventListener("click", () => {
+  ctx.fillStyle = document.getElementById("color").value;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+});
+
+// *****************
+let erasing = false;
+const eraserButton = document.getElementById("eraser");
+const pencilButton = document.getElementById("pencil");
+eraserButton.addEventListener("click", () => {
+  toggleEraser();
+});
+pencilButton.addEventListener("click", () => {
+  togglePencil();
+});
+
+function toggleEraser() {
+  erasing = !erasing;
+  if (erasing) {
+    ctx.globalCompositeOperation = "destination-out";
+    setEraserCursor();
+    canvas.addEventListener("mousemove", draw);
+  } else {
+    ctx.globalCompositeOperation = "source-over";
+    canvas.classList.remove("canvas-eraser");
+    canvas.removeEventListener("mousemove", draw);
+  }
+}
+
+function togglePencil() {
+  erasing = false;
+  ctx.globalCompositeOperation = "source-over";
+  canvas.style.cursor = "default";
+}
